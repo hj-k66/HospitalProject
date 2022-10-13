5dd8569912ccbc77c1092df3fd23eb81cd82766e
heejeong <hjx66@naver.com>
2022-10-13 14:32:30 +0900

    [refactor] parse에서 subdivision 구하도록 변경

--- src/main/java/com/line/parser/HospitalParser.java
+++ src/main/java/com/line/parser/HospitalParser.java
@@ -3,10 +3,24 @@ package com.line.parser;
 import com.line.domain.Hospital;
 
 public class HospitalParser implements Parser<Hospital>{
+
+    private String getSubdivision(String name){
+        String[] sublist = { "외과","내과", "소아", "피부", "성형", "정형외과", "척추", "교정", "산부인과",
+                "관절", "봉합", "화상", "골절", "영유아", "안과", "가정의학과", "비뇨기과", "치과"};
+        for(String s : sublist){
+            if(name.contains(s)){
+                return s;
+            }
+        }
+        return "";
+    }
     @Override
     public Hospital parse(String str) {
         str = str.replaceAll("\"", "");
         String[] splitted = str.split(",");
-        return new Hospital(splitted[0], splitted[1],splitted[2],Integer.parseInt(splitted[6]),splitted[10],splitted[7]);
+
+        String subdivision = getSubdivision(splitted[7]);
+
+        return new Hospital(splitted[0], splitted[1],splitted[2],Integer.parseInt(splitted[6]),splitted[10],subdivision);
     }
 }
