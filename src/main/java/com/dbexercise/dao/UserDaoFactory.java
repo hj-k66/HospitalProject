8d76fdfbfd54c0f8a84b373ad2c96b8afbe32ecd
heejeong <hjx66@naver.com>
2022-10-19 13:47:51 +0900

    [refactor] Spring 적용

--- src/main/java/com/dbexercise/dao/UserDaoFactory.java
+++ src/main/java/com/dbexercise/dao/UserDaoFactory.java
@@ -1,14 +1,19 @@
 package com.dbexercise.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class UserDaoFactory {
     //관계 설정 기능
     //조립하기
+    @Bean
     public UserDao2 awsUserDao(){
         AwsConnectionMaker awsConnectionMaker = new AwsConnectionMaker();
         UserDao2 userDao2 = new UserDao2(awsConnectionMaker);
         return userDao2;
     }
-
+    @Bean
     public UserDao2 localUserDao(){
         UserDao2 userDao2 = new UserDao2(new LocalConnectionMaker());
         return userDao2;
